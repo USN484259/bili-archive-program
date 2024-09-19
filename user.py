827a0184e7360f19127fa2b17a294d5a1befdf6e
USN484259 <USN484259@outlook.com>
2024-09-20 00:43:11 +0800

    user video collections
    1. implement user video collections fetching
    2. implement user video downloading

--- user.py
+++ user.py
@@ -10,6 +10,7 @@ from collections import ChainMap
 import core
 import runtime
 import network
+from video_collection import fetch_user_collections, gather_resources_from_collectons
 
 # constants
 
@@ -39,21 +40,28 @@ async def get_user_brief_info(sess, uid_list):
 	return {str(u["mid"]): u for u in info}
 
 
+async def fetch_image(sess, url, img_root, stall = None):
+	try:
+		img_match = img_pattern.fullmatch(url)
+		if not img_match:
+			return
+		img_name = img_match.group(1)
+		logger.debug("fetching image %s", img_name)
+		img_path = os.path.join(img_root, img_name)
+		if os.path.isfile(img_path):
+			return
+		stall and await stall()
+		await network.fetch(sess, url, img_path)
+	except Exception:
+		logger.exception("failed to fetch image")
+
+
 async def recursive_save_images(sess, path, table, stall = None):
 	for v in table.values():
-		try:
-			if type(v) is dict:
-				await recursive_save_images(sess, path, v, stall)
-			elif type(v) is str:
-				img_match = img_pattern.fullmatch(v)
-				if img_match:
-					img_name = img_match.group(1)
-					logger.debug("fetching image %s", img_name)
-					img_path = os.path.join(path, img_name)
-					stall and await stall()
-					await network.fetch(sess, v, img_path)
-		except Exception:
-			logger.exception("failed to fetch image")
+		if type(v) is dict:
+			await recursive_save_images(sess, path, v, stall)
+		elif type(v) is str:
+			await fetch_image(sess, v, path, stall)
 
 
 # methods
@@ -94,23 +102,36 @@ async def fetch_users(sess, uid_list, stall = None):
 # entrance
 
 async def main(args):
-	stall = runtime.Stall()
-	user_root = args.dir or runtime.subdir("user")
-	async with network.session() as sess:
-		user_map = await fetch_users(sess, args.inputs, stall)
+	if args.download:
+		import video
 
+	async with network.session() as sess:
+		stall = runtime.Stall()
+		user_root = args.dir or runtime.subdir("user")
+
+		user_map = await fetch_users(sess, args.inputs, stall)
+		bv_table = set()
 		fetched_user = 0
 		for uid, info in user_map.items():
 			fetch_status = False
 			try:
-				logger.debug(info)
+				collection = await fetch_user_collections(sess, uid, stall)
+				info["videos"] = collection
+				bv_t, img_table = gather_resources_from_collectons(collection)
+				bv_table |= bv_t
+
+				# TODO fetch dynamics
 				with core.locked_path(user_root, uid) as uid_path:
 					info_path = os.path.join(uid_path, "info.json")
 					with core.staged_file(info_path, "w", rotate = True) as f:
 						json.dump(info, f, indent = '\t', ensure_ascii = False)
+
+					# TODO save images in seperate task
 					await recursive_save_images(sess, uid_path, info, stall)
 
-				# TODO fetch dynamics and videos
+					for img_url in img_table:
+						await fetch_image(sess, img_url, uid_path, stall)
+
 				fetched_user += 1
 				fetch_status = True
 			except Exception:
@@ -120,9 +141,13 @@ async def main(args):
 
 		logger.info("finished user download %d/%d", fetched_user, len(args.inputs))
 
+		if args.download:
+			await video.batch_download(sess, bv_table, args.dir or runtime.subdir("video"), mode = args.mode, prefer = args.prefer, reject = args.reject)
+
 
 if __name__ == "__main__":
-	args = runtime.parse_args(("network", "auth", "dir"), [
+	args = runtime.parse_args(("network", "auth", "dir", "bandwidth", "video_mode", "prefer"), [
 		(("inputs",), {"nargs" : '+'}),
+		(("--download",), {"action": "store_true"}),
 	])
 	asyncio.run(main(args))
---
+++ video_collection.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python3
+
+import logging
+
+import core
+import runtime
+import network
+
+# constants
+
+USER_VIDEO_LIST_URL = "https://api.bilibili.com/x/space/wbi/arc/search"
+USER_COLLECTIONS_URL = "https://api.bilibili.com/x/polymer/web-space/seasons_series_list"
+# USER_CHANNEL_LIST_URL = "https://api.bilibili.com/x/space/channel/video"
+
+USER_SERIES_LIST_URL = "https://api.bilibili.com/x/series/archives"
+USER_SEASON_LIST_URL = "https://api.bilibili.com/x/polymer/web-space/seasons_archives_list"
+
+# static objects
+
+logger = logging.getLogger("bili_arch.collection")
+
+# helper functions
+
+async def get_video_page(sess, uid, page):
+	info = await network.request(sess, "GET", USER_VIDEO_LIST_URL, wbi_sign = True, params = {"mid": uid, "pn": page})
+	return info
+
+
+async def get_collections(sess, uid, page):
+	info = await network.request(sess, "GET", USER_COLLECTIONS_URL, wbi_sign = True, params = {"mid": uid, "page_num": page, "page_size": 20})
+	return info
+
+
+async def get_series_page(sess, uid, sid, page):
+	info = await network.request(sess, "GET", USER_SERIES_LIST_URL, wbi_sign = True, params = {"mid": uid, "series_id": sid, "pn": page})
+	return info
+
+
+async def get_season_page(sess, uid, sid, page):
+	info = await network.request(sess, "GET", USER_SEASON_LIST_URL, wbi_sign = True, params = {"mid": uid, "season_id": sid, "page_num": page})
+	return info
+
+
+# methods
+
+async def fetch_user_videos(sess, uid, stall = None):
+	video_info = None
+	video_count = None
+	video_list = []
+	page_index = 1
+	try:
+		while video_count is None or len(video_list) < video_count:
+			stall and await stall()
+			logger.debug("fetching page %d", page_index)
+			video_page = await get_video_page(sess, uid, page_index)
+
+			if video_info is None:
+				video_info = video_page.get("list").get("tlist")
+				video_count = video_page.get("page").get("count")
+				logger.info("user %s, videos %d", uid, video_count)
+
+			vlist = video_page.get("list").get("vlist")
+			if not vlist:
+				logger.warning("empty video page %d, stop here", page_index)
+				break
+			video_list += vlist
+			page_index += 1
+	except Exception:
+		logger.exception("failed to fetch user videos")
+
+	logger.info("fetched video list %d/%d", len(video_list), video_count or 0)
+	return {"info": video_info, "count": video_count, "list": video_list}
+
+
+async def fetch_user_collection_videos(sess, meta, stall = None):
+	get_func = None
+	sid = None
+	if "series_id" in meta:
+		get_func = get_series_page
+		sid = meta.get("series_id")
+	elif "season_id" in meta:
+		get_func = get_season_page
+		sid = meta.get("season_id")
+	else:
+		raise RuntimeError("unknown collection type")
+
+	uid = meta.get("mid")
+	video_count = meta.get("total")
+	video_list = []
+	page_index = 1
+	try:
+		while len(video_list) < video_count:
+			stall and await stall()
+			logger.debug("fetching page %d", page_index)
+			video_page = await get_func(sess, uid, sid, page_index)
+			videos = video_page.get("archives")
+			if not videos:
+				logger.warning("empty video page %d, stop here", page_index)
+				break
+			video_list += videos
+			page_index += 1
+	except Exception:
+		logger.exception("failed to fetch collection videos")
+
+	logger.info("fetched video collection %d/%d", len(video_list), video_count)
+	return video_list
+
+
+async def fetch_user_collection_list(sess, uid, stall = None):
+	try:
+		# TODO handle collection_list more than one page
+		stall and await stall()
+		collections = await get_collections(sess, uid, 1)
+		season_list = collections.get("items_lists").get("seasons_list")
+		series_list = collections.get("items_lists").get("series_list")
+
+		logger.info("fetched collection list %d, %d", len(series_list), len(season_list))
+		return {
+			"seasons_list": [s.get("meta") for s in season_list],
+			"series_list": [s.get("meta") for s in series_list],
+		}
+	except Exception:
+		logger.exception("failed to fetch collection list")
+		return {}
+
+
+async def fetch_user_collections(sess, uid, stall = None):
+	logger.info("fetching video collections for user %s", uid)
+	result = {
+		"series": [],
+		"seasons": [],
+	}
+	result["videos"] = await fetch_user_videos(sess, uid, stall)
+	collection_list = await fetch_user_collection_list(sess, uid, stall)
+
+	for series in collection_list.get("series_list", {}):
+		try:
+			video_list = await fetch_user_collection_videos(sess, series, stall)
+			result["series"].append({
+				"info": series,
+				"list": video_list,
+			})
+		except Exception:
+			logger.exception("failed to fetch series")
+
+	for season in collection_list.get("seasons_list", {}):
+		try:
+			video_list = await fetch_user_collection_videos(sess, season, stall)
+			result["seasons"].append({
+				"info": season,
+				"list": video_list,
+			})
+		except Exception:
+			logger.exception("failed to fetch season")
+
+	return result
+
+
+def gather_resources_from_collectons(collection):
+	bv_table = set()
+	img_table = set()
+
+	def gather_from_list(l):
+		if not l:
+			return
+		for v in l:
+			bvid = v.get("bvid")
+			img_url = v.get("pic")
+			if bvid:
+				bv_table.add(bvid)
+			if img_url:
+				img_table.add(img_url)
+
+	gather_from_list(collection.get("videos", {}).get("list"))
+
+	for s in collection.get("series", []):
+		img_url = s.get("info", {}).get("cover")
+		if img_url:
+			img_table.add(img_url)
+		gather_from_list(s.get("list"))
+
+	for s in collection.get("seasons", []):
+		img_url = s.get("info", {}).get("cover")
+		if img_url:
+			img_table.add(img_url)
+		gather_from_list(s.get("list"))
+
+	return bv_table, img_table
